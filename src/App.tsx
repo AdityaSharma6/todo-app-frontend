38f721c814c306a50e65c863358ed88de624350a
Aditya Sharma <aditya_sharma_@hotmail.com>
2021-02-02 12:47:38 -0500

    Added handler and a request function to create a new list. Added aux function to get todays date to ensure each list is created with a unique title

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,18 @@ export const App: FunctionComponent = () => {
         });
     };
 
+    const createTodoListHandler = async () => {
+        const response = await createTodoListInDatabase();
+        console.log(response);
+        setTodoList({
+            _id: response._id,
+            title: response.title,
+            creationDate: response.creationDate,
+            description: response.description,
+            todoItemsCollection: [],
+        });
+    };
+
     return (
         <Main
             key={todoList._id}
@@ -35,10 +47,12 @@ export const App: FunctionComponent = () => {
             updateTodoListTitleToRequest={setTodoListTitleToRequest}
             todoListNameToSearch={todoListTitleToRequest}
             todoList={todoList}
+            createTodoListHandler={createTodoListHandler}
         />
     );
 };
 
+// GET readOneList
 const fetchTodoListTitle = async (todoListTitle: string): Promise<TodoListType> => {
     try {
         const endpoint = API + `list/${todoListTitle}`;
@@ -52,6 +66,7 @@ const fetchTodoListTitle = async (todoListTitle: string): Promise<TodoListType> 
     }
 };
 
+// GET readAllItemsFromList
 const fetchTodoItems = async (todoListId: string): Promise<TodoItemType[]> => {
     try {
         const endpoint = API + `items/?_listId=${todoListId}`;
@@ -62,4 +77,39 @@ const fetchTodoItems = async (todoListId: string): Promise<TodoItemType[]> => {
         console.log(error);
         return error;
     }
+};
+
+// POST createOneList
+const createTodoListInDatabase = async (): Promise<TodoListType> => {
+    const endpoint = API + 'list/';
+    try {
+        const date = getTodaysDate();
+        console.log(date);
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: `Fake Title #${date}`,
+                description: '',
+            }),
+        });
+        const { data } = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+
+export const getTodaysDate = () => {
+    const today = new Date();
+    const day = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const year = today.getDate();
+    const hour = today.getHours();
+    const minutes = today.getMinutes();
+    const seconds = today.getSeconds();
+    return year + '-' + month + '-' + day + '-' + hour + '-' + minutes + '-' + seconds;
 };
